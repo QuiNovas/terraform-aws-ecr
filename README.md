81b21dc52244a16039ceabe93ad0eb3cd30cb0e7
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-08-22 22:09:22 -0500

    First checkin
    After extraction from QuiNovas/terraform-modules

---
+++ README.md
@@ -0,0 +1,11 @@
+# terraform-aws-ecr
+
+This module creates an Elastic Container Registry (ECR)
+
+## Authors
+
+Module managed by Quinovas (https://github.com/QuiNovas)
+
+## License
+
+Apache License, Version 2.0, January 2004 (http://www.apache.org/licenses/). See LICENSE for full details.
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "cross_account_users" {
+  type        = "list"
+  description = "A list of cross-account users, often the root user of those accounts, to grant access to the ECR repository."
+  default     = []
+}
+
+variable "name" {
+  description = "Name of the repository."
+  type        = "string"
+}
