76e3ccae577ac02469ee8e73c43817b62cee9ac1
rajhansh-seyfert <kumarrajhanshgit@gmail.com>
2019-07-02 14:39:13 +0530

    added flatten function in list

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ data "aws_caller_identity" "current" {
 
 data "aws_iam_policy_document" "repo" {
   statement {
-    actions = [
+    actions = flatten([
       "ecr:BatchCheckLayerAvailability",
       "ecr:BatchGetImage",
       "ecr:DescribeImages",
@@ -16,7 +16,7 @@ data "aws_iam_policy_document" "repo" {
       "ecr:GetDownloadUrlForLayer",
       "ecr:GetRepositoryPolicy",
       "ecr:ListImages",
-    ]
+    ])
     principals {
       identifiers = [
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
